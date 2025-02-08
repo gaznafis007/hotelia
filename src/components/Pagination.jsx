32a99087b09093c53f1ba77a41ede6b204a763ee
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-09 00:05:45 +0600

    pagination system init

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+export default function Pagination({ currentPage, totalPages }) {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1)
+
+  return (
+    <div className="flex justify-center space-x-2 mt-8">
+      {pages.map((page) => (
+        <Link
+          key={page}
+          href={`/hotels?page=${page}`}
+          className={`px-4 py-2 rounded ${currentPage === page ? "bg-blue-600 text-white" : "bg-gray-200"}`}
+        >
+          {page}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
