d09be593342cf4dfb162e0274c1bf5e872d8384e
unknown <gazinafisrafi.gnr@gmail.com>
2025-02-09 20:46:35 +0600

    hotel delete and update api created

--- src/app/api/hotels/[id]/route.js
+++ src/app/api/hotels/[id]/route.js
@@ -12,6 +12,36 @@ export const GET = async (req, {params}) =>{
         const result = await hotelCollection.findOne(query);
         return NextResponse.json(result);
     }catch(err){
-        return NextResponse.json({error: err}, {status: 500})
+        return NextResponse.json({error: err.message}, {status: 500})
+    }
+}
+export const DELETE = async (req, {params}) =>{
+    try{
+        const {id} = await params;
+        const query = {_id: new ObjectId(id)};
+        const db = await connectDB();
+        const hotelCollection = await db.collection('hotels');
+        const result = await hotelCollection.deleteOne(query);
+        return NextResponse.json(result)
+    }catch(err){
+        return NextResponse.json({error: err.message}, {status: 500})
+    }
+}
+export const PUT = async (req,{params}) =>{
+    try{
+        const {id} = await params;
+        const hotel = await req.json();
+        const db = await connectDB();
+        const hotelCollection = db.collection('hotels');
+        const query = {_id: new ObjectId(id)};
+        const updatedDoc = {
+            $set:{
+                ...hotel
+            }
+        }
+        const result = await hotelCollection.updateOne(query, updatedDoc);
+        return NextResponse.json(result)
+    }catch(err){
+        return NextResponse.json({error: err.message}, {status: 500})
     }
 }
